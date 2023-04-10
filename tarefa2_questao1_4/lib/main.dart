9abd6f9b4150b8f7508e534e2474a2be9cdf405d
flaviogui <flavioglaydson-pc@hotmail.com>
2023-04-10 20:59:31 -0300

    Criando classes

--- tarefa2_questao1_4/lib/main.dart
+++ tarefa2_questao1_4/lib/main.dart
@@ -1,31 +1,52 @@
 import 'package:flutter/material.dart';
 
+class NewNavBar extends StatelessWidget {
+  NewNavBar();
+
+  void botaoFoiTocado(int index) {
+    print("Tocaram no botão $index");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(onTap: botaoFoiTocado, items: const [
+      BottomNavigationBarItem(
+        label: "Cafés",
+        icon: Icon(Icons.coffee_outlined),
+      ),
+      BottomNavigationBarItem(
+          label: "Cervejas", icon: Icon(Icons.local_drink_outlined)),
+      BottomNavigationBarItem(label: "Nações", icon: Icon(Icons.flag_outlined))
+    ]);
+  }
+}
+
+class TelaInicial extends StatelessWidget {
+  TelaInicial();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      Expanded(
+        child: Text("La Fin Du Monde - Bock - 65 ibu"),
+      ),
+      Expanded(
+        child: Text("Sapporo Premiume - Sour Ale - 54 ibu"),
+      ),
+      Expanded(
+        child: Text("Duvel - Pilsner - 82 ibu"),
+      ),
+    ]);
+  }
+}
+
 void main() {
   MaterialApp app = MaterialApp(
       theme: ThemeData(primarySwatch: Colors.deepPurple),
       home: Scaffold(
         appBar: AppBar(title: Text("Dicas")),
-        body: Column(children: [
-          Expanded(
-            child: Text("La Fin Du Monde - Bock - 65 ibu"),
-          ),
-          Expanded(
-            child: Text("Sapporo Premiume - Sour Ale - 54 ibu"),
-          ),
-          Expanded(
-            child: Text("Duvel - Pilsner - 82 ibu"),
-          )
-        ]),
-        bottomNavigationBar: BottomNavigationBar(items: const [
-          BottomNavigationBarItem(
-            label: "Cafés",
-            icon: Icon(Icons.coffee_outlined),
-          ),
-          BottomNavigationBarItem(
-              label: "Cervejas", icon: Icon(Icons.local_drink_outlined)),
-          BottomNavigationBarItem(
-              label: "Nações", icon: Icon(Icons.flag_outlined))
-        ]),
+        body: TelaInicial(),
+        bottomNavigationBar: NewNavBar(),
       ));
   runApp(app);
 }
