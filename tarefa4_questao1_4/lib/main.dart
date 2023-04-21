76ee123aa548fffe59f17791b351f0baaf87b067
flaviogui <flavioglaydson-pc@hotmail.com>
2023-04-20 21:01:43 -0300

    PASSO 6

--- tarefa4_questao1_4/lib/main.dart
+++ tarefa4_questao1_4/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-// PASSO 4 e 5
+// PASSO 6
 void main() {
   MyApp app = MyApp();
 
@@ -61,12 +61,24 @@ class DataBodyWidget extends StatelessWidget {
         columns: [
           DataColumn(
               label: Expanded(
-            child: Text('Descrição',
-                style: TextStyle(fontStyle: FontStyle.italic)),
+            child: Text("Nome", style: TextStyle(fontStyle: FontStyle.italic)),
+          )),
+          DataColumn(
+              label: Expanded(
+            child:
+                Text("Estilo", style: TextStyle(fontStyle: FontStyle.italic)),
+          )),
+          DataColumn(
+              label: Expanded(
+            child: Text("IBU", style: TextStyle(fontStyle: FontStyle.italic)),
           ))
         ],
         rows: objects
-            .map((obj) => DataRow(cells: [DataCell(Text('$obj'))]))
+            .map((obj) => DataRow(cells: [
+                  DataCell(Text(obj["name"])),
+                  DataCell(Text(obj["style"])),
+                  DataCell(Text(obj["ibu"]))
+                ]))
             .toList());
   }
 }
